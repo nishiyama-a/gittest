584cd06e96c3d9ec0c2bc6b069dc32f0f8dd2a56
{aki <{nishiyama@queserser.co.jp}>
2023-04-13 16:07:38 +0900

    初期状態

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="stylesheet" href="css/assets.css">
+</head>
+
+<body>
+
+    <div class="flx">
+        <div class="sec">リモートのmasterです</div>
+        <div class="sec">リモートのdevelopです</div>
+        <div class="sec">リモートのreleaseです</div>
+        <div class="sec">リモートの作業です</div>
+    </div>
+    <div class="flx">
+        <div class="sec">ローカルのmasterです</div>
+        <div class="sec">ローカルのdevelopです</div>
+        <div class="sec">ローカルのreleaseです</div>
+        <div class="sec">ローカルの作業です</div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
